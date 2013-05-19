23662cb069ce3a802bd771fee884b5a2ddc377bf
Maciej Pasternacki <maciej@pasternacki.net>
2013-05-19 04:46:00 +0200

    sqlite: no readline needed

--- config/software/sqlite.rb
+++ config/software/sqlite.rb
@@ -9,13 +9,14 @@ source :url => "http://www.sqlite.org/2013/sqlite-autoconf-#{vtag}.tar.gz",
 relative_path "sqlite-autoconf-#{vtag}"
 
 env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
   "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
   "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
 }
 
 build do
-  command "./configure --prefix=#{install_dir}/embedded", :env => env
+  command "./configure --prefix=#{install_dir}/embedded --disable-readline",
+          :env => env
   command "make -j #{max_build_jobs}", :env => env
   command "make install"
 end
